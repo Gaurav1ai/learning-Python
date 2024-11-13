c8919fc8dbbca81d8fc396ac5438e52c45aaf6d1
shishodia1ai@gmail.com <shishodia1ai@gmail.com>
2024-11-13 11:50:16 +0530

    Output veriable

---
+++ localrepo/output variables.py
@@ -0,0 +1,19 @@
+#Output veriable
+x = "python is hard"
+print(x)
+x="python"
+y="is"
+z="hard"
+print(x,y,z)
+print(x+y+z)
+x= 1
+y=2
+print(x+y)
+x="python"
+y=2
+print(x,y)
+#run time error int + string 
+# print(x+y)
+# Float and intiger can add
+x=2.2
+print(x+y)
