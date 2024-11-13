dcd2baf2ddb8fd625891bcecf1a73c15361eb427
shishodia1ai@gmail.com <shishodia1ai@gmail.com>
2024-11-13 11:14:59 +0530

    unpack collection

--- localrepo/hi.py
+++ localrepo/hi.py
@@ -1,19 +1,34 @@
-x,y,k= 1,2 ,10
-print(x+y)
-z =x+y
-print(z)
-x = z+y
-z+=x
-print(z)
-x+=z
-z= x+z+y
-y+=z
-print(y)
-x=y=z=300
+#multiple Variable
+# x,y,k= 1,2 ,10
+# print(x+y)
+# z =x+y
+# print(z)
+# x = z+y
+# z+=x
+# print(z)
+# x+=z
+# z= x+z+y
+# y+=z
+# print(y)
+
+#one value to multiple veriable
+# x=y=z=300
+# print(x)
+# print(y)
+# print(z)
+# k+=x
+# print(k)
+# k-=y
+# print(k)
+
+#Unpack Collection
+
+fruits=["apple","banana","Cherry"]
+x,y,z = fruits
+m=n=o = fruits
 print(x)
 print(y)
 print(z)
-k+=x
-print(k)
-k-=y
-print(k)
+print(m)
+print(n)
+print(o)
