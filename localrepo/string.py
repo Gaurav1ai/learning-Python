314fd136a41b6e0574cc0a33b982a3ed4be35a3a
shishodia1ai@gmail.com <shishodia1ai@gmail.com>
2024-11-13 15:45:11 +0530

    string

--- localrepo/string.py
+++ localrepo/string.py
@@ -1,7 +1,51 @@
-#string in multiple lines
-x = """we
-are
-become
-best in
-python for sure"""
-print(x)
+# #string in multiple lines
+# x = """we
+# are
+# become
+# best in
+# python for sure"""
+# print(x)
+
+# #slicing string
+# x = "hii","bye","hello","dead"
+# print(x[-4:-3])
+# print(x[:2])
+# print(x[2:])
+# y = "gaurav"
+# print(y[2])
+# print(y[1:3])
+
+# #string split
+# a = "hello,world!,namasta"
+# b = a.split(",")
+# print(a.upper())
+# print(a.lower())
+# print(a.replace("h","f"))
+# x,y,z=b
+# print(b)
+# print(a)
+# print(x)
+# print(y)
+# print(z)
+# print("hello ",7)
+
+# #formate string
+# age = 21
+# txt = f"My age is {age}"
+# print(age)
+# print(txt)
+
+# #Escape Characters
+# s= "hellooo\rworld"
+# j= "hello\nworld"
+# print(s)
+# print(j)
+
+
+# txt = 'It\'s alright.'
+# print(txt) 
+
+
+i = 2
+print(bool(i==3))
+print(bool(i==2))
