addaeb75f53c982aa77982bf3a5d10fa9d05a0cf
shishodia1ai@gmail.com <shishodia1ai@gmail.com>
2024-11-13 12:05:27 +0530

    Global veriable

--- Global veriable.py
+++ Global veriable.py
@@ -1,6 +1,18 @@
-x = 2
-def myfun():
-    x= 100
+i = int(input("Enter Your Cholice 1 or 0=  "))
+if(i==1):
+    x = 2
+    def myfun():
+        x= 100
+        print(x)
+    myfun()
     print(x)
-myfun()
-print(x)
+else:
+    #global veriables
+    x = 4
+    print(x)
+    def myfun():
+       global x
+       x=10
+       print(x)
+    myfun()
+    print(x)
