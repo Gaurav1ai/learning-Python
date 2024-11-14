dddce52d78927211ed7499e24c21cde99f40e00a
shishodia1ai@gmail.com <shishodia1ai@gmail.com>
2024-11-14 12:49:58 +0530

    Tuple

---
+++ localrepo/Tuple.py
@@ -0,0 +1,62 @@
+# i = ("apple","orange","kiwi","grapes")
+# print(type(i))
+# print(i[1])
+# print(i[-1])
+# print(i[2:4])
+# print(i[-1:-3])
+# print(i[:4])
+# print(i[2:])
+
+# if "orange" in i:
+#     print("Yes,'orange is present in tuple!'")
+    
+#update the tuple
+# i = ("apple","orange","kiwi","grapes")
+i = ("apple","orange","grapes")
+# j=list(i)
+# j.append("banana")
+# i=tuple(j)
+# print(i)
+
+##add object
+# j = ("cherry",)
+# i +=j
+# print(i)
+
+# #Remove
+# j=list(i)
+# j.remove("kiwi")
+# i=tuple(j)
+# print(i)
+
+##Using delete
+# del i
+# print(i)
+
+# # Unpack Tuple
+# (green,yellow,red)= i
+# print(green)
+# print(yellow)
+# print(red)
+
+# j =(1,2,3,4,5,6,7,8,9,0)
+# (a,*b,c) = j
+# print(a)
+# print(b)
+# print(c)
+
+
+# #Join tuple
+# j = ("a","o","k","g")
+
+# new= i+j
+# print(new)
+# second = j*2
+# print(second)
+
+# #count and index
+# j =(1,3,2,5,3,5,2,6,7,6,8,9)
+# n = i.count("apple")
+# print(n)
+# print(j.index(8))
+
